6e08260e845ace03fb7313adf453ed3f08b68778
Ramachandra-2096 <ramachandraudupa2004@gmail.com>
2023-10-07 06:43:06 +0530

    completed

--- Relations/myapp/models.py
+++ Relations/myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.\
+class person(models.Model):
+    name = models.CharField(max_length=60)
+    phone = models.IntegerField()
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.name
+    
+class Adhar(models.Model):
+    person = models.OneToOneField("person",on_delete=models.CASCADE)
+    signature = models.TextField()
+    adhar_no = models.TextField(max_length=20)
+    def __str__(self):
+        return str(self.person)
